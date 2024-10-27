8784c8babfb94bce78484b69cf147445e17563d9
Hr_28215 <harshalborad2003@gmail.com>
2024-10-27 17:04:01 +0530

    Refactor content paths in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
     darkMode: ["class"],
     content: [
 		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
+		"./Components/**/*.{js,ts,jsx,tsx}",
 	  ],
   theme: {
   	extend: {
