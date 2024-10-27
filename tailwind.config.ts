dea2ab0aa33e7da731a69671fe56d4d5efa4424a
Hr_28215 <harshalborad2003@gmail.com>
2024-10-27 17:29:19 +0530

    Update tailwindcss version to 3.4.14 and refactor content paths in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,13 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
     darkMode: ["class"],
-    content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./Components/**/*.{js,ts,jsx,tsx}",
+	content: [
+		"./app/**/*.{js,ts,jsx,tsx}", 
+		"./pages/**/*.{js,ts,jsx,tsx}", 
+		"./components/**/*.{js,ts,jsx,tsx}", 
+		
+		// Or if using `src` directory: 
+		"./src/**/*.{js,ts,jsx,tsx}", 
 	  ],
   theme: {
   	extend: {
